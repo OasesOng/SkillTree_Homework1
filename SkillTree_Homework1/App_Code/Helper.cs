68e7e74934a77e2c49d927a4f52bec22b62f6e5d
OasesOng <fish.0829@outlook.com>
2016-04-25 15:56:45 +0800

    add Helper Package - IsCurrentPage

---
+++ SkillTree_Homework1/App_Code/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SkillTree_Homework1.App_Code
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// 檢查目前之頁面的Routes
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="controllerName"></param>
+        /// <param name="actionName"></param>
+        /// <returns></returns>
+        public static bool IsCurrentPage(this HtmlHelper helper, string controllerName, string actionName)
+        {
+            var currentControllerName = (string)helper.ViewContext.RouteData.Values["controller"];
+            var currentActionName = (string)helper.ViewContext.RouteData.Values["action"];
+
+            if (currentControllerName.Equals(controllerName, StringComparison.CurrentCultureIgnoreCase)
+                && currentActionName.Equals(actionName, StringComparison.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+    }
+}
