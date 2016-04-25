cd2e1495e9a8d72be8caad9b7551f5836c925c85
OasesOng <fish.0829@outlook.com>
2016-04-25 16:03:17 +0800

    依據功課要求 - 修改屬性

--- SkillTree_Homework1/Models/ViewModels/MoneyViewModel.cs
+++ SkillTree_Homework1/Models/ViewModels/MoneyViewModel.cs
@@ -4,30 +4,35 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.Web.Mvc;
+using SkillTree_Homework1.Models.Validator;
 
 namespace SkillTree_Homework1.Models.ViewModels
 {
     public class MoneyViewModel
     {
         [DisplayName("分類")]
-        [Required]
+        [Required(ErrorMessage ="必須選擇分類")]
         public int category { get; set; }
 
         [DisplayName("金額")]
-        [Required]
+        [Required(ErrorMessage = "必須記錄金額")]
+        [Range(0, int.MaxValue, ErrorMessage = "只能輸入正整數")]
         public int money { get; set; }
 
         [DisplayName("日期")]
-        [Required]
+        [Required(ErrorMessage = "必須填寫日期")]
+        //使用ValidationDate
+        [ValidationDate(ErrorMessage = "「日期」不得大於今天")]
         [DataType(DataType.Date)]
         public DateTime date { get; set; }
 
 
         [DisplayName("備註")]
-        [Required]
+        [Required(ErrorMessage = "必須填寫備註")]
+        [StringLength(100, ErrorMessage = "「備註」最大輸入100個字元")]
         public string remark { get; set; }
-
     }
 
-   
+
 }
